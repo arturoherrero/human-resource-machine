47279d99fe534870024b5d372573bb2e271dd29e
Arturo Herrero <arturo.herrero@gmail.com>
2016-11-06 15:18:49 +0000

    Level 21

---
+++ 21-zero-terminated-sum.size.speed.asm
@@ -0,0 +1,33 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 21-zero-terminated-sum - SIZE 10/10 - SPEED 68/72 --
+
+    JUMP     c
+a:
+    COPYFROM 0
+b:
+    OUTBOX  
+c:
+    INBOX   
+    JUMPZ    b
+d:
+    COPYTO   0
+    INBOX   
+    JUMPZ    a
+    ADD      0
+    JUMP     d
+
+
+DEFINE LABEL 0
+eJwzZGBgmGWx+5i0x+5j+712Htnps2k/t9+KHfF+s9dv9524Yo93x8KLzqVTLG0TWmdZeJZ4mltneZqb
+pH4y14pztJIK7rBndqp1/mkl4fnJlCHovTHQOAbTuPfGpnGt843iZ6ytS9p2qDLlwMkTaUfP7s06fi46
+f/MBpsJFW4KL9JMCi3k8A4tfG4D0GNZ/Mr1Tt2k/iH294Y3h/k6F8Phey8zlk6SCe+b8tGpc8FTn6oLX
+BvrzeTyN5pmld87N6TWdu2n/3bnHzzGMglEwCsgGAOEqVlI;
+
+DEFINE LABEL 5
+eJxzZmBgmGDzyVTa45Mpt99X8x8hDA7fQtldvcIEfRaGSgUvCZEPU3Z1L7a0TWi1sy6caGddN8vapmlu
+VEDTXNvo6hlTosqnAo1gOJH20+pEWsGEwvSSySwFNTN/FdfM9Cyrmj63rHQKSP5Apnb83DLteBC7MP21
+wYTqF3og9pmW57pnWoT9SlvVY0pbk9rk2lrng8Sl2x9oMEx8qPl70lOdeVO/mn+YxuryYZpC+Lypugmh
+Ey0zszqts0InBlW/mFk6xXxO01yQng3rHmnlrPhnd3QZl7vqYosM9UUBleqLsnoqF1dMK1naPG/H6ulr
+ItfPWrdiY+/S99uiG5/s0oqbsIvd1W3bZ7PATW8MGUbBKBiBAAB+s3B7;
+
+
